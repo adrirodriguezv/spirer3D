590ed2043b6949e7a736becb1185a34e3741b35a
adrianRodriguez77 <adrian.rodriguez.vegas.7e6@itb.cat>
2025-02-17 19:59:20 +0100

    refactor: Improve navigation bar styling and structure

--- S3DR/src/components/NavegadorDark.vue
+++ S3DR/src/components/NavegadorDark.vue
@@ -20,23 +20,39 @@ onMounted(() => {
   <header>
     <nav :class="{ 'scrolled': isScrolled }" id="nav">
       <ul>
-        <div><RouterLink :class="{'scrolled': isScrolled}" :to="`/`"><img src="/Logo3D.png"></RouterLink></div>
-        <li><RouterLink :class="{'scrolled': isScrolled}" :to="`/`">Inicio</RouterLink></li>
-        <li><RouterLink :class="{'scrolled': isScrolled}" :to="`/productos`">Productos</RouterLink></li>
-        <li><RouterLink :class="{'scrolled': isScrolled}" :to="`/precio`">Precios</RouterLink></li>
-        <li><RouterLink :class="{'scrolled': isScrolled}" :to="`/personalizar`">Personalizar</RouterLink></li>
-        <li><RouterLink :class="{'scrolled': isScrolled}" :to="`/contacto`">Contacto</RouterLink></li>
-            
+        <div>
+          <RouterLink :class="{ 'scrolled': isScrolled }" :to="`/`"><img src="/Logo3D.png"></RouterLink>
+        </div>
+        <li>
+          <RouterLink :class="{ 'scrolled': isScrolled }" :to="`/`">Inicio</RouterLink>
+        </li>
+        <li>
+          <RouterLink :class="{ 'scrolled': isScrolled }" :to="`/productos`">Productos</RouterLink>
+        </li>
+        <li>
+          <RouterLink :class="{ 'scrolled': isScrolled }" :to="`/precio`">Precios</RouterLink>
+        </li>
+        <li>
+          <RouterLink :class="{ 'scrolled': isScrolled }" :to="`/personalizar`">Personalizar</RouterLink>
+        </li>
+        <li>
+          <RouterLink :class="{ 'scrolled': isScrolled }" :to="`/contacto`">Contacto</RouterLink>
+        </li>
+
       </ul>
-      <ul><div class="cliente">
+      <ul>
+        <div class="cliente">
           <li><img src="../assets/img/perfil.png"></li>
-        </div>  </ul>
+          <li><img src="../assets/img/cesta.png"></li>
+        </div>
+      </ul>
     </nav>
   </header>
 </template>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
 * {
   border: 1px #545454;
   font-family: 'Poppins', sans-serif;
@@ -55,57 +71,69 @@ nav {
   position: fixed;
   top: 0;
   left: 50%;
-  transform: translateX(-50%); /* Centra el nav horizontalmente */
-  width: 100%; /* Ajusta el ancho según el contenido */
+  transform: translateX(-50%);
+  /* Centra el nav horizontalmente */
+  width: 100%;
+  /* Ajusta el ancho según el contenido */
   z-index: 1000;
-  background: transparent; /* Fondo transparente por defecto */
-  transition: background-color 0.3s ease, border-radius 0.3s ease; /* Transición suave */
+  background: transparent;
+  /* Fondo transparente por defecto */
+  transition: background-color 0.3s ease, border-radius 0.3s ease;
+  /* Transición suave */
   padding: 1.5%;
 }
 
 nav.scrolled {
-  background: #efefef; /* Fondo oscuro con opacidad */
+  background: #efefef;
+  /* Fondo oscuro con opacidad */
 
 }
 
 ul {
   display: flex;
   justify-content: space-around;
-  width: 50%; /* Hace que los elementos se distribuyan uniformemente */
+  width: 50%;
+  /* Hace que los elementos se distribuyan uniformemente */
   list-style: none;
   padding: 2px 0;
   margin-left: 10%;
-  
+
 }
 
 li {
   font-weight: 500;
   margin-top: 3%;
   transition: transform 0.3s ease, box-shadow 0.3s ease;
-  
+
 }
 
 a {
   text-decoration: none;
   color: #545454;
-  transition: color 0.3s ease; /* Transición suave al hacer hover */
+  transition: color 0.3s ease;
+  /* Transición suave al hacer hover */
 }
 
 li:hover {
   transform: scale(1.1);
 }
-.cliente{
+
+.cliente {
   display: flex;
 
 }
-.cliente img{
+
+.cliente img {
   width: 30px;
 }
+
 @media (max-width: 800px) {
   nav {
     width: 90%;
-    left: 5%; /* Mantiene el margen lateral */
-    transform: none; /* Sin transformación */
+    left: 5%;
+    /* Mantiene el margen lateral */
+    transform: none;
+    /* Sin transformación */
   }
 
   img {
